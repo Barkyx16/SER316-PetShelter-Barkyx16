7502251b09daae62c0a5b459d4f64d4bf87b4cd2
Alex Rafalski <alexrafalski@Alexs-MacBook-Pro.local>
2026-02-16 18:23:32 -0700

    A4: add passing JUnit test with required naming convention

---
+++ app/src/test/java/edu/asu/ser316/petshelter/AppTest.java
@@ -0,0 +1,13 @@
+package edu.asu.ser316.petshelter;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+
+class AppTest {
+
+    @Test
+    void T_Q1_basicFunctionalityPasses() {
+        assertTrue(true);
+    }
+}
+
